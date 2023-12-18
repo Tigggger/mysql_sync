19079aad941c9a5ee8707d9cb26730d79e7cc6f8
Tigggger <github@prestoncouriers.co.uk>
2023-12-18 21:07:44 +0000

    Update function.php

--- function.php
+++ function.php
@@ -1,5 +1,5 @@
     function remote_curl_get($query) {
-        $url = 'https://yourdomain.com/?password=xxxxxxxxxxxxxxxxx&query=';
+        $url = 'https://yourbackupdomain.com/?password=xxxxxxxxxxxxxxxxx&query=';
         $url.= urlencode($query);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HEADER, 0);
